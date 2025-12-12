ed8669718e18166881041d3297c5c1f63f36087f
LoH-lu <139378145+LoH-lu@users.noreply.github.com>
2025-12-12 13:50:21 +0100

    Update __init__.py

--- netbox_ip_permissions_synchronization/__init__.py
+++ netbox_ip_permissions_synchronization/__init__.py
@@ -1,10 +1,16 @@
 from netbox.plugins import PluginConfig
-import sys
 import os
 
-# Import version from root
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from _version import __version__, __author__, __author_email__, __description__, __license__
+_version_file = os.path.join(os.path.dirname(__file__), '..', '_version.py')
+_version_data = {}
+with open(_version_file, encoding="utf-8") as f:
+    exec(f.read(), _version_data)
+
+__version__ = _version_data["__version__"]
+__author__ = _version_data["__author__"]
+__author_email__ = _version_data["__author_email__"]
+__description__ = _version_data["__description__"]
+__license__ = _version_data["__license__"]
 
 
 class Config(PluginConfig):
