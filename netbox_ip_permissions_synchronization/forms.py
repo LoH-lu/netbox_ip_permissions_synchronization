2a40414f4ee30198105d94534088fdd8efed1ec4
LoH-lu <139378145+LoH-lu@users.noreply.github.com>
2025-12-12 13:36:34 +0100

    Update forms.py

--- netbox_ip_permissions_synchronization/forms.py
+++ netbox_ip_permissions_synchronization/forms.py
@@ -4,4 +4,3 @@ from django import forms
 
 class IPPermissionsSyncForm(forms.Form):
     sync_all = forms.BooleanField(required=False)
-    selected_ips = forms.CharField(required=False, widget=forms.HiddenInput())
