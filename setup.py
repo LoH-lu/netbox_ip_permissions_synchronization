6c4b1e9cdf2cf8c3debf170c218d1ce381842ec9
LoH-lu <139378145+LoH-lu@users.noreply.github.com>
2025-12-12 13:34:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 from setuptools import setup
+import sys
+import os
+
+# Add root directory to path to import _version
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+from _version import __version__, __author__, __author_email__, __description__, __license__
 
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='netbox_ip_permissions_synchronization',
-    version='0.0.14',
-    description='Syncing permissions on IP Addresses with those from their corresponding Prefix in NetBox',
+    version=__version__,
+    description=__description__,
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/LoH-lu/netbox_ip_permissions_synchronization/',
-    author='Loris Henrion',
-    author_email='loris_henrion@bce.lu',
-    license='GPL-3.0',
+    author=__author__,
+    author_email=__author_email__,
+    license=__license__,
     packages=["netbox_ip_permissions_synchronization"],
     package_data={"netbox_ip_permissions_synchronization": ["templates/netbox_ip_permissions_synchronization/*.html"]},
     zip_safe=False
