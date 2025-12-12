dad656709bf244ba85ba76d4c445314b3eb0bc2e
LoH-lu <139378145+LoH-lu@users.noreply.github.com>
2025-12-12 13:49:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,32 @@
-from setuptools import setup
-import sys
 import os
+from setuptools import setup
 
-# Add root directory to path to import _version
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-from _version import __version__, __author__, __author_email__, __description__, __license__
+# Read version and metadata from _version.py
+_version_file = os.path.join(os.path.dirname(__file__), "_version.py")
+_version_data = {}
+with open(_version_file, encoding="utf-8") as f:
+    exec(f.read(), _version_data)
 
-with open('README.md', encoding='utf-8') as f:
+__version__ = _version_data["__version__"]
+__author__ = _version_data["__author__"]
+__author_email__ = _version_data["__author_email__"]
+__description__ = _version_data["__description__"]
+__license__ = _version_data["__license__"]
+
+with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='netbox_ip_permissions_synchronization',
+    name="netbox_ip_permissions_synchronization",
     version=__version__,
     description=__description__,
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/LoH-lu/netbox_ip_permissions_synchronization/',
+    long_description_content_type="text/markdown",
+    url="https://github.com/LoH-lu/netbox_ip_permissions_synchronization/",
     author=__author__,
     author_email=__author_email__,
     license=__license__,
     packages=["netbox_ip_permissions_synchronization"],
     package_data={"netbox_ip_permissions_synchronization": ["templates/netbox_ip_permissions_synchronization/*.html"]},
-    zip_safe=False
+    zip_safe=False,
 )
