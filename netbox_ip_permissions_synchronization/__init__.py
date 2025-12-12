231656a53bb05492452db9eb571256f32984fb96
LoH-lu <139378145+LoH-lu@users.noreply.github.com>
2025-12-12 13:35:41 +0100

    Update __init__.py

--- netbox_ip_permissions_synchronization/__init__.py
+++ netbox_ip_permissions_synchronization/__init__.py
@@ -1,15 +1,19 @@
-## __init__.py
 from netbox.plugins import PluginConfig
+import sys
+import os
+
+# Import version from root
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from _version import __version__, __author__, __author_email__, __description__, __license__
 
 
 class Config(PluginConfig):
     name = 'netbox_ip_permissions_synchronization'
     verbose_name = 'NetBox IP Permissions Synchronization'
-    description = 'Syncing permissions on IP Addresses with those from their corresponding Prefix in NetBox'
-    version = '0.0.14'
-    author = 'Loris Henrion'
-    author_email = 'loris_henrion@bce.lu'
-    base_url = 'ip-permissions'
+    description = __description__
+    version = __version__
+    author = __author__
+    author_email = __author_email__
 
 
 config = Config
